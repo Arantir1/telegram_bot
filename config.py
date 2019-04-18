d51967e7e374cd457f4aefb94c815fb0f4773817
Eugene <eugene.kobyak@gmail.com>
2019-04-18 12:56:24 +0300

    migrate to postgresql

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ token = '455559594:AAGCLJfO04AXjeuolSbyEjregszmPp0d-QE'
 secret = "a3ed8746-6a1c-42e8-a544-7357bbc93572"
 
 class Database ():
-  host = 'argman.mysql.pythonanywhere-services.com'
-  user = 'argman'
-  password='west0000'
-  database='argman$dictionary'
+  host = 'ec2-54-247-85-251.eu-west-1.compute.amazonaws.com'
+  user = 'sonskqfqihofvq'
+  password='a6827bb71c1a8316e43da36a08c4bd302f0bb870214bd12c4bd6e6c4712ca654'
+  database='dbi7isql5o217v'
