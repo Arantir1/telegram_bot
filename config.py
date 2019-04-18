aec43282b749bb74531841df8d32c973cceca42e
Eugene <eugene.kobyak@gmail.com>
2019-04-18 12:44:24 +0300

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# Bot configs
+
+token = '455559594:AAGCLJfO04AXjeuolSbyEjregszmPp0d-QE'
+secret = "a3ed8746-6a1c-42e8-a544-7357bbc93572"
+
+class Database ():
+  host = 'argman.mysql.pythonanywhere-services.com'
+  user = 'argman'
+  password='west0000'
+  database='argman$dictionary'
