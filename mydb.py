7e23b7edf31c6b802816eafd100d994d54450049
Evgen <evgenkb1@gmail.com>
2019-06-17 17:02:44 +0300

    change datetime to timestamp type

--- mydb.py
+++ mydb.py
@@ -15,7 +15,7 @@ def create_db_table():
         print("Table already exist!")
     except psycopg2.errors.UndefinedTable:
         connection.rollback()
-        create_table_sql = "CREATE TABLE dictionary (word VARCHAR, cid VARCHAR, last_repeat DATETIME, iteration INTEGER, next_repeat DATETIME);"
+        create_table_sql = "CREATE TABLE dictionary (word VARCHAR, cid VARCHAR, last_repeat TIMESTAMP, iteration INTEGER, next_repeat TIMESTAMP);"
         cursor.execute(create_table_sql)
         connection.commit()
         print("Table has created!")
