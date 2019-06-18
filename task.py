b9b32805aa67a96e0be6cf36f6917628bdaad9bd
Evgen <evgenkb1@gmail.com>
2019-06-18 13:39:05 +0300

    debug scheduler2

--- task.py
+++ task.py
@@ -16,9 +16,9 @@ def set_scheduler(remember_words, message):
         print("Schedule installed")
 
 
-bot = telebot.TeleBot(config.token, threaded=False)
-data  = mydb.get_words()
+# bot = telebot.TeleBot(config.token, threaded=False)
+# data  = mydb.get_words()
 
-for cid in data.keys():
-    bot.send_message(cid, "Time to repeat")
-    bot.send_message(cid, ''.join(str(word + ', ') for word in data[cid]))
+# for cid in data.keys():
+#     bot.send_message(cid, "Time to repeat")
+#     bot.send_message(cid, ''.join(str(word + ', ') for word in data[cid]))
