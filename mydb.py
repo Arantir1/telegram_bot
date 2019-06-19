b8ac212c335dc6671784fbd5dca814518c75565f
Evgen <evgenkb1@gmail.com>
2019-06-19 12:27:10 +0300

    change psycopg2 to sqlalchemy

--- mydb.py
+++ mydb.py
@@ -1,70 +1,73 @@
-import psycopg2
+# import psycopg2
 import datetime
 import config
+from sqlalchemy import create_engine, exc
+class Mydb():
 
-def create_db_table():
-    connection = psycopg2.connect(host=config.Database.host, user=config.Database.user,
-                                 password=config.Database.password, database=config.Database.database)
-    # cursor = connection.cursor(buffered=True)
-    cursor = connection.cursor()
-    try:
-        select_sql = "SELECT * FROM dictionary;"
-        cursor.execute(select_sql)
-        res = cursor.fetchone()
-        print("Res is: ",res)
-        print("Table already exist!")
-    except psycopg2.errors.UndefinedTable:
-        connection.rollback()
-        create_table_sql = "CREATE TABLE dictionary (word VARCHAR, cid VARCHAR, last_repeat TIMESTAMP, iteration INTEGER, next_repeat TIMESTAMP);"
-        cursor.execute(create_table_sql)
-        connection.commit()
-        print("Table has created!")
-    connection.close()
+    __engine = None
 
+    def __init__(self):
+        self.__engine = create_engine("postgresql://\
+                            %(user)s:\
+                            %(password)s@\
+                            %(host)s:\
+                            %(port)s\
+                            /%(database)s",
+            {
+                'user':config.Database.user,
+                'password':config.Database.password,
+                'host':config.Database.host,
+                'port':config.Database.port,
+                'database':config.Database.database
+            }
+        ) 
 
-def insert_word(message):
-    connection = psycopg2.connect(host=config.Database.host, user=config.Database.user,
-                                 password=config.Database.password, database=config.Database.database)
-    cursor = connection.cursor()
-    last_dt = datetime.datetime.now()
-    new_dt = last_dt + datetime.timedelta(days=1)
-    cursor.execute("INSERT INTO dictionary VALUES (%(word)s, \
-                                                %(cid)s, \
-                                                %(last_repeat)s, \
-                                                %(iteration)s, \
-                                                %(next_repeat)s);",
-    {'word': message.text, 'cid': message.chat.id, 'last_repeat': last_dt, 'iteration': 1, 'next_repeat': new_dt})
-    connection.commit()
-    connection.close()
+    def create_db_table(self):
+        try:
+            connection = self.__engine.connect()
+            res = connection.execute("SELECT * FROM dictionary;")
+            print("Res is: ",res)
+            print("Table already exist!")
+        except exc.NoSuchTableError:
+            create_table_sql = "CREATE TABLE dictionary (word VARCHAR, cid VARCHAR, last_repeat TIMESTAMP, iteration INTEGER, next_repeat TIMESTAMP);"
+            connection.execute(create_table_sql)
+            print("Table has created!")
+        connection.close()
 
-def get_words_by_cid(cid):
-    connection = psycopg2.connect(host=config.Database.host, user=config.Database.user,
-                                 password=config.Database.password, database=config.Database.database)
-    cursor = connection.cursor()
-    cursor.execute("SELECT word from dictionary WHERE cid = %(cid)s;", {'cid': cid})
-    words = []
-    word = cursor.fetchone()
-    while word is not None:
-        words.append(word[0])
-        word = cursor.fetchone()
-    connection.close()
-    return words
+    def insert_word(self, message):
+        connection = self.__engine.connect()
+        last_dt = datetime.datetime.now()
+        new_dt = last_dt + datetime.timedelta(days=1)
+        connection.execute("INSERT INTO dictionary VALUES (:word, \
+                                                    :cid, \
+                                                    :last_repeat, \
+                                                    :iteration, \
+                                                    :next_repeat);",
+        word=message.text, cid=message.chat.id, last_repeat=last_dt, iteration=1, next_repeat=new_dt)
+        connection.close()
 
-def get_words():
-    connection = psycopg2.connect(host=config.Database.host, user=config.Database.user,
-                                 password=config.Database.password, database=config.Database.database)
-    cursor = connection.cursor()
-    res = {}
-    cids = []
-    cursor.execute("SELECT DISTINCT cid FROM dictionary;")
-    row = cursor.fetchone()
-    # print(row)
-    while row is not None:
-        cids.append(row[0])
-        row = cursor.fetchone()
-    # print (cids)
-    for cid in cids:
-        res.update({cid: get_words_by_cid(cid)})
-    connection.close()
-    return res
+    def get_words_by_cid(self, cid):
+        connection = self.__engine.connect()
+        result = connection.execute("SELECT word from dictionary WHERE cid = :cid;", cid=cid)
+        words = []
+        word = result.fetchone()
+        while word is not None:
+            words.append(word[0])
+            word = result.fetchone()
+        connection.close()
+        return words
+
+    def get_words(self):
+        connection = self.__engine.connect()
+        res = {}
+        cids = []
+        result = connection.execute("SELECT DISTINCT cid FROM dictionary;")
+        row = result.fetchone()
+        while row is not None:
+            cids.append(row[0])
+            row = result.fetchone()
+        for cid in cids:
+            res.update({cid: self.get_words_by_cid(cid)})
+        connection.close()
+        return res
 
