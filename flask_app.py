0dab0c7629ac62c2e44d4c5b25f5aeac76af64ed
Evgen <evgenkb1@gmail.com>
2019-08-15 12:14:33 +0300

    add empty_library message with sad smile 3d try

--- flask_app.py
+++ flask_app.py
@@ -82,7 +82,7 @@ def show_words(message):
         bot.send_message(message.chat.id, "Ваши слова:")
         bot.send_message(message.chat.id, ''.join(str(word + ', ') for word in words))
     else:
-        bot.send_message(message.chat.id, "Ваш словарь пуст " + u"\UF09F9894")
+        bot.send_message(message.chat.id, "Ваш словарь пуст :pensive:")
 
 @bot.message_handler(commands=['stop'])
 def command_stop(message):
