5078b2ca9787e5ab44437d366ce964ee6b2797e4
Evgen <evgenkb1@gmail.com>
2019-08-16 14:10:22 +0300

    add functions to manage schedule_jobs

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ import telebot
 import config
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers import SchedulerNotRunningError
-
+from apscheduler.jobstores.base import BaseJobStore
 class MyScheduler:
 
     __scheduler = None
@@ -20,6 +20,21 @@ class MyScheduler:
     def __del__(self):
         self.__scheduler.remove_all_jobs()
         self.__scheduler.shutdown()
+
+    def is_job_running(self, id):
+        return any(job.id == id for job in BaseJobStore.get_all_jobs())
+
+    def add_job(self, hour, minute, id, remember_words, message):
+        self.__scheduler.add_job(func=remember_words, trigger="cron", args=[message], hour=hour, minute=minute, id=id, replace_existing=True)
+        print("Job {1} added".format(id))    
+
+    def remove_job(self, id):
+        self.__scheduler.remove_job(job_id=id)
+        print("Job {1} removed".format(id))  
+
+    def start_job_now(self, id):
+        print("Starting the {1} job...".format(id))
+        next((job for job in BaseJobStore.get_all_jobs() if str(job.id) == id), None).func()
 
     def set_scheduler(self, remember_words, message):
         print("Setting schedule...")
