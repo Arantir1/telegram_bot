d5ed57d1d51772f13eb9e9c4c484a2559f64a84f
Evgen <evgenkb1@gmail.com>
2019-08-20 14:40:38 +0300

    fix commands

--- flask_app.py
+++ flask_app.py
@@ -25,7 +25,7 @@ def telegram_webhook():
 def set_markup():
     user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
     user_markup.row('/add_word', '/remove_word')
-    user_markup.row('run_job', 'set_job', 'remove_job')
+    user_markup.row('/run_job', '/set_job', '/remove_job')
     user_markup.row('/show_words')
     return user_markup
 
